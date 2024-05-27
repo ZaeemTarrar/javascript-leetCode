a762d8c9562f3e79075fa3312be69bd2baf281ab
Zaeem Hassan Tarar <zaeemhassantarar@Zaeems-MBP-2.realtek>
2024-05-27 19:13:14 +0800

    easy

--- Easy/Two Sum/run.js
+++ Easy/Two Sum/run.js
@@ -12,7 +12,7 @@ const logTitle = "Two Sum";
  * No. of times, the test method is intended to be called.
  * @type {number}
  */
-const testRounds = 1000;
+const testRounds = 10000;
 
 /**
  * List of arguments as input array for solution functions.
@@ -22,23 +22,35 @@ const inputs = [
   [[2, 7, 11, 15], 9],
   [[3, 1, 4, 11], 7],
   [[1, 4, 2, 9], 10],
+  [[3, 2, 4], 6],
+  [[3, 3], 6],
 ];
 
-console.log(`<=== ${logTitle} ===> \n`);
+console.log(`<=== ${logTitle} ===> `);
 
 /**
- * Uni-Test with Output Verification
+ * Test with Output Verification
  */
-testSolution(Sol1.twoSum, inputs, true, "[UniTest-Solution1] ");
+console.log("\n[UniTest-Solution1]: ");
+testSolution(Sol1.twoSum, inputs, true);
+console.log("\n[UniTest-Solution2]: ");
+testSolution(Sol2.twoSum, inputs, true);
 console.log();
-testSolution(Sol2.twoSum, inputs, true, "[UniTest-Solution2] ");
-console.log();
+
+/**
+ * Uni-Testing
+ * Test Configuration: 1 Rounds
+ * Solution1: ~ 0.016ms, 0.121mb
+ * Solution2: ~ 0.005ms, 0.001mb
+ */
+multiTest(Sol1.twoSum, inputs, false, "Solution1", 1);
+multiTest(Sol2.twoSum, inputs, false, "Solution2", 1);
 
 /**
  * Multi-Testing
- * Test Configuration: 1000 Rounds
- * Solution1: ~ 0.845ms
- * Solution2: ~ 0.631ms
+ * Test Configuration: 10000 Rounds
+ * Solution1: ~ 8.075ms, 0mb
+ * Solution2: ~ 1.272ms, 0.729mb
  */
 multiTest(Sol1.twoSum, inputs, false, "Solution1", testRounds);
 multiTest(Sol2.twoSum, inputs, false, "Solution2", testRounds);
--- utils/test.js
+++ utils/test.js
@@ -1,4 +1,35 @@
 /**
+ * Converts bytes to mega bytes
+ * @param {number} bytes - Any value in bytes
+ * @returns {string} "123 MB"
+ */
+const bytesToMegaBytes = (bytes) => {
+  return `${bytes * 1e-6} MB`;
+};
+
+/**
+ * Calculates the difference between two memory usage stats and
+ * converts the result object values from bytes to mega bytes.
+ * @param {any} mem1 - process.memoryUsage()
+ * @param {any} mem2 - process.memoryUsage()
+ * @returns {{
+ *    rss: string,
+ *    heapTotal: string,
+ *    heapUsage: string,
+ *    external: string,
+ * }}
+ */
+const memUsageDiffInMb = (mem1, mem2) => {
+  if (!mem1 || !mem2) return null;
+  return {
+    rss: bytesToMegaBytes(mem2.rss - mem1.rss),
+    heapTotal: bytesToMegaBytes(mem2.heapTotal - mem1.heapTotal),
+    heapUsed: bytesToMegaBytes(mem2.heapUsed - mem1.heapUsed),
+    external: bytesToMegaBytes(mem2.external - mem1.external),
+  };
+};
+
+/**
  * Takes your solution and tests it with the given input/inputs.
  * @param {function} solution
  * @param {array} inputs
@@ -23,14 +54,24 @@ function testSolution(solution, inputs = [], log = false, preText = "") {
  * @param {number} rounds - No. of times, method if to be tested
  * @return {void}
  */
-function multiTest(solution, inputs = [], log = false, label = "Solution", rounds = 1) {
+function multiTest(
+  solution,
+  inputs = [],
+  log = false,
+  label = "Solution",
+  rounds = 1
+) {
+  const mem1 = process.memoryUsage();
   if (log) console.log(`=> ${label}:`);
-  let timeLabel = `=> ${label}[${rounds} rounds]`;
-  console.time(timeLabel);
+  const start = performance.now();
   for (let i = 0; i < rounds; i++) {
     testSolution(solution, inputs, log, `[${i}] -> `);
   }
-  console.timeEnd(timeLabel);
+  const end = performance.now();
+  const mem2 = process.memoryUsage();
+  console.log(`${label} [${rounds} rounds]: `);
+  console.log(`-> Performance: ${(end - start).toFixed(3)}ms`);
+  console.log(`-> Memory Usage: `, memUsageDiffInMb(mem1, mem2));
   console.log();
 }
 
